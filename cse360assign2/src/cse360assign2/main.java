42e4e4b0bb271b3f8980a3457008d3cf864f3c53
Haley <Haley@192.168.0.8>
2019-02-24 12:25:50 -0700

    initial version

---
+++ cse360assign2/src/cse360assign2/main.java
@@ -0,0 +1,17 @@
+package cse360assign2;
+
+public class main {
+	
+	public static void main(String[] args) 
+	{
+		Calculator c1 = new Calculator();
+		c1.add(1);
+		c1.add(2);
+		c1.add(2);
+		c1.subtract(2);
+		c1.multiply(2);
+		c1.divide(2);
+		c1.divide(0);
+	}
+
+}
