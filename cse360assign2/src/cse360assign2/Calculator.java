c90fba92937312d900d0df09a97fbb01d314b910
Haley <Haley@192.168.0.8>
2019-02-24 10:44:22 -0700

    Initial version Calculator

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,71 @@
+
+/*	Author: Haley Hanson
+ *	Class ID: 395
+ *	Assignment: 2
+ *  Description: 
+ *  <p>
+ *  
+ * */
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Gets the totals
+	 * 
+	 * @return  	the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds an integer
+	 * 
+	 * @param value	the integer to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtracts an integer
+	 * 
+	 * @param value	the integer to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiplies an integer
+	 * 
+	 * @param value	the integer to be multiplied
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides an integer
+	 * 
+	 * @param value	the integer to be divided
+	 */
+	public void divide (int value) {
+		
+	}
+	/**
+	 * Prints the calculator's history
+	 * 
+	 * @return  	the string contain the history 
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
