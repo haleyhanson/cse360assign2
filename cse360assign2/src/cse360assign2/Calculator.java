c751b63712d257e39a8776e4cee71f1c6fc1c603
Haley <Haley@192.168.0.8>
2019-02-24 11:29:16 -0700

    second version

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -2,70 +2,91 @@
 /*	Author: Haley Hanson
  *	Class ID: 395
  *	Assignment: 2
- *  Description: 
+ *  Description: This file contains the Calculator class which acts as a calculator.
+ *  A variable keeps track of the calculator's history and can be modified by 
+ *  performing an operation with one integer at a time. 
  *  <p>
+ *  The Calculator class contains 5 methods. The first four allow the private total 
+ *  number to be modified with the following operations: adding, subtracting, 
+ *  multiplying or dividing. The last method provides a history of the operations
+ *  that took place. 
  *  
  * */
 package cse360assign2;
 
-public class Calculator {
+public class Calculator 
+{
 
 	private int total;
 	
-	public Calculator () {
+	public Calculator () 
+	{
 		total = 0;  // not needed - included for clarity
 	}
 	
 	/**
-	 * Gets the totals
+	 * Gets the total
 	 * 
 	 * @return  	the total
 	 */
-	public int getTotal () {
-		return 0;
+	public int getTotal () 
+	{
+		return total;
 	}
 	
 	/**
-	 * Adds an integer
+	 * Adds an integer to the total variable
 	 * 
-	 * @param value	the integer to be added
+	 * @param value		the integer that is added to total
 	 */
-	public void add (int value) {
-		
+	public void add (int value) 
+	{
+		total += value;
 	}
 	
 	/**
-	 * subtracts an integer
+	 * Subtracts an integer from the total 
 	 * 
-	 * @param value	the integer to be subtracted
+	 * @param value		the integer that is subtracted from total
 	 */
-	public void subtract (int value) {
-		
+	public void subtract (int value) 
+	{
+		total -= value;
 	}
 	
 	/**
-	 * multiplies an integer
+	 * Multiplies the total by an integer 
 	 * 
-	 * @param value	the integer to be multiplied
+	 * @param value		the integer that multiplies the total
 	 */
-	public void multiply (int value) {
-		
+	public void multiply (int value) 
+	{
+		total *= value;
 	}
 	
 	/**
-	 * Divides an integer
+	 * Divides the total by an integer
 	 * 
-	 * @param value	the integer to be divided
+	 * @param value		the integer that divides the total
 	 */
-	public void divide (int value) {
-		
+	public void divide (int value) 
+	{
+		if(value == 0)
+		{
+			total = 0;
+		}
+		else
+		{
+			total /= value;
+		}
 	}
 	/**
-	 * Prints the calculator's history
+	 * Prints a string of the calculator's history
 	 * 
-	 * @return  	the string contain the history 
+	 * @return  	 string containing history of operations
 	 */
-	public String getHistory () {
+	public String getHistory () 
+	{
 		return "";
 	}
 }
